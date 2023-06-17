5276bb5806538087571036fd566e80c6b1b1f5ae
X_Shuler_X <egor.pisanov@gmail.com>
2023-06-17 18:04:20 +0300

    Правки по таск2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -4,36 +4,49 @@
 32679 -> 6*/
 
 Console.WriteLine("Введите число a ");
-int a = int.Parse(Console.ReadLine()!);
+int a = Math.Abs(int.Parse(Console.ReadLine()!));
 
-if (a < 1000 && a >= 100)
+if (a > 100000)
 {
-/*int a1 = a / 10;*/
-int a1 = a % 10;
-
-Console.WriteLine($"Третья цифра числа {a} = {a1}");
+    Console.WriteLine($"Число слишком большое");
 }
 else
 {
-Console.WriteLine($"Третьей цифры нет");
-};
+    if (a < 100)
+    {
+        Console.WriteLine("Нету 3 цифры");
+    }
+    else
+    {
+        if (a < 1000 && a >= 100)
+        {
+            /*int a1 = a / 10;*/
+            int a1 = a % 10;
 
-if (a < 10000 && a >= 1000)
-{
-int a1 = a % 100;
-int a2 = a1 / 10;
+            Console.WriteLine($"Третья цифра числа {a} = {a1}");
 
-Console.WriteLine($"Третья цифра числа {a} = {a2}");
+        }
+        else
+        {
+            if (a < 10000 && a >= 1000)
+            {
+                int a1 = a % 100;
+                int a2 = a1 / 10;
+
+                Console.WriteLine($"Третья цифра числа {a} = {a2}");
+
+            }
+            else
+            {
+                if (a < 100000 && a >= 10000)
+                {
+                    int a1 = a % 1000;
+                    int a2 = a1 / 100;
+
+                    Console.WriteLine($"Третья цифра числа {a} = {a2}");
+
+                }
+            }
+        }
+    }
 }
-
-if (a < 100000 && a >= 10000)
-{
-int a1 = a % 1000;
-int a2 = a1 / 100;
-
-Console.WriteLine($"Третья цифра числа {a} = {a2}");
-}
-else
-{
-Console.WriteLine($"Число слишком большое");
-};
